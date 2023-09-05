580761b50e8ec515c57c4e8334f114e334d59df0
Francisco Wladison <francisco.wladison62@aluno.ifce.edu.br>
2023-09-05 11:37:10 -0300

    att

---
+++ ManipulacaoDeListas/ManipulacaoLista(Pop)/App.js
@@ -0,0 +1,9 @@
+// Criando uma lista (array) com alguns elementos
+const minhaLista = [1, 2, 3, 4, 5];
+
+// Utilizando o método pop() para remover o último elemento
+let elementoRemovido = minhaLista.pop();
+
+// Exibindo o elemento removido e a lista atualizada
+console.log("Elemento removido: " + elementoRemovido);
+console.log("Lista atualizada: " + minhaLista);
