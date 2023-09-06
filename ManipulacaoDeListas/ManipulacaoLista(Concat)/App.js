893c0f31d025bfe2f396e11be112fc46d7d449ca
Francisco Wladison <francisco.wladison62@aluno.ifce.edu.br>
2023-09-05 22:05:14 -0300

    att

---
+++ ManipulacaoDeListas/ManipulacaoLista(Concat)/App.js
@@ -0,0 +1,6 @@
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+// Com esse método, estamos juntando as duas Arrays
+console.log(array3);
+// Imprime Array ["a", "b", "c", "d", "e", "f"]
