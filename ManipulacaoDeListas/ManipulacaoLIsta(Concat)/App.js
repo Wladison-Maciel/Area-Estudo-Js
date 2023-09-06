e2d9cb1a455620200bbf2ac5bb299bf07531fcba
Wladison Maciel <125041870+Wladison-Maciel@users.noreply.github.com>
2023-09-05 22:03:46 -0300

    Delete ManipulacaoDeListas/ManipulacaoLIsta(Concat) directory

--- ManipulacaoDeListas/ManipulacaoLIsta(Concat)/App.js
+++
@@ -1,6 +0,0 @@
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
-const array3 = array1.concat(array2);
-// Com esse método, estamos juntando as duas Arrays
-console.log(array3);
-// Imprime Array ["a", "b", "c", "d", "e", "f"]
