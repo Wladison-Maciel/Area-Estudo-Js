32fa845be326aeb65c9115750964d27d1b538c09
Francisco Wladison <francisco.wladison62@aluno.ifce.edu.br>
2023-09-05 22:01:53 -0300

    att

---
+++ ManipulacaoDeListas/ManipulacaoLista(Reverse)/App.js
@@ -0,0 +1,5 @@
+let myArray = ["one", "two", "three"];
+
+myArray.reverse();
+// Com este método iremos inverter a ordem da lista
+console.log(myArray); // ['three', 'two', 'one']
---
+++ ManipulacaoDeListas/ManipulacaoLista(Sort)/App.js
@@ -0,0 +1,8 @@
+// Criar um array de números desordenados
+let numeros = [5, 2, 9, 1, 5];
+
+// Usar o método sort para classificar o array em ordem crescente
+numeros.sort();
+
+// Exibir o array ordenado no console
+console.log(numeros); // Saída: [1, 2, 5, 5, 9]
