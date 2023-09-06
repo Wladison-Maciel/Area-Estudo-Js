33f348365dddf0b787e2665203ba4346cbb33a10
Francisco Wladison <francisco.wladison62@aluno.ifce.edu.br>
2023-09-05 21:40:06 -0300

    att

---
+++ ManipulacaoDeListas/ManipulacaoLista(Push)/App.js
@@ -0,0 +1,10 @@
+// Criar um array vazio
+let minhaArray = [];
+
+// Adicionar elementos ao array usando o método push
+minhaArray.push(10);       // Adiciona o número 10 ao final do array
+minhaArray.push("Olá");   // Adiciona a string "Olá" ao final do array
+minhaArray.push(true);    // Adiciona o valor booleano true ao final do array
+
+// Exibir o array resultante no console
+console.log(minhaArray); // Saída: [10, "Olá", true]
