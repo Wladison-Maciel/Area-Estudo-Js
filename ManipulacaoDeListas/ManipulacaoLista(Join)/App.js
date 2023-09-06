2f7f62b0a8d1512c6e52f348d77d61a57f83afc4
Francisco Wladison <francisco.wladison62@aluno.ifce.edu.br>
2023-09-05 21:34:49 -0300

    att

---
+++ ManipulacaoDeListas/ManipulacaoLista(Join)/App.js
@@ -0,0 +1,9 @@
+// Criando um array de exemplo
+const arrayExemplo = ["Maçã", "Banana", "Pera", "Uva"];
+
+// Usando o método join() para unir os elementos do array em uma única string
+const stringUnida = arrayExemplo.join(" - "); // Você pode especificar o separador desejado
+
+// Exibindo a string unida
+console.log("Array original:", arrayExemplo);
+console.log("String unida:", stringUnida);
